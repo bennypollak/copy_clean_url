b8120ddace3e8b79f910b47dc6c843c31b98a463
Benny Pollak <yo372002@yahoo.com>
2018-11-17 13:01:38 -0500

    new version w/shortcuts

--- background.js
+++ background.js
@@ -2,52 +2,71 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// https://github.com/mdn/webextensions-examples
-// /blob/master/context-menu-copy-link-with-types/
-// clipboard-helper.js
+// https://github.com/mdn/webextensions-examples/blob/master/context-menu
+// -copy-link-with-types/clipboard-helper.js
 const copyToClipboard = text => {
-  const onCopy = event => { document.removeEventListener("copy", onCopy, true);
-    event.stopImmediatePropagation();
-    event.preventDefault();
-    event.clipboardData.setData("text/plain", text);
-  };
+    const onCopy = event => {
+        document.removeEventListener("copy", onCopy, true);
+        event.stopImmediatePropagation();
+        event.preventDefault();
+        event.clipboardData.setData("text/plain", text);
+    };
 
-  document.addEventListener("copy", onCopy, true);
-  document.execCommand("copy");
+    document.addEventListener("copy", onCopy, true);
+    document.execCommand("copy");
 };
 
+
 function copyURL(url) {
-  chrome.tabs.getSelected(null, function(tab) {
-    var clean = url.replace(/\?.*/, "")
-    console.log("copied '%s'", clean);
-    //chrome.tabs.update(tab.id, {
-      //url: clean
-    //});
-    copyToClipboard(clean)
-  });
+    chrome.tabs.getSelected(null, function (tab) {
+        var clean = url.replace(/\?.*/, "")
+
+        console.log("copied '%s'", clean)
+        copyToClipboard(clean)
+    });
 }
 
-chrome.runtime.onConnect.addListener(function(port) {
-  var tab = port.sender.tab;
-  // This will get called by the content script we execute in
-  // the tab as a result of the user pressing the browser action.
-  port.onMessage.addListener(function(info) {
-    chrome.tabs.getSelected(null, function(tab) {
-      copyURL(tab.url)
+chrome.runtime.onConnect.addListener(function (port) {
+        console.log('port:', port);
+    var tab = port.sender.tab;
+    // This will get called by the content script we execute in
+    // the tab as a result of the user pressing the browser action.
+    port.onMessage.addListener(function (info) {
+        chrome.tabs.getSelected(null, function (tab) {
+            copyURL(tab.url)
+        });
     });
-  });
 });
 
-// Called when the user clicks on the browser action icon.
-//chrome.browserAction.onClicked.addListener(function(tab) {
-//  // We can only inject scripts to find the title on pages loaded with http
-//  // and https so for all other pages, we don't ask for the title.
-//  if (tab.url.indexOf("http:") != 0 &&
-//    tab.url.indexOf("https:") != 0) {} else {
-//    console.log('copying %s', tab.url)
-//    copyURL(tab.url)
-//    chrome.tabs.executeScript(null, {
-//      file: "content_script.js"
-//    });
-//  }
+//chrome.pageAction.onClicked.addListener(function(tab) {
+//    console.log('PA %j', tab)
 //});
+// Listen for any changes to the URL of any tab.
+//chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
+//    chrome.pageAction.show(tabId);
+//});
+
+// Called when the user clicks on the browser action icon.
+//chrome.browserAction.onClicked.addListener(function (tab) {
+//    // We can only inject scripts to find the title on pages loaded with http
+//    // and https so for all other pages, we don't ask for the title.
+//    chrome.extension.getBackgroundPage().console.log('clicked %j', tab)
+//    if (tab.url.indexOf("http:") != 0 &&
+//            tab.url.indexOf("https:") != 0) {
+//    } else {
+//        console.log('copying %s', tab.url)
+//        copyURL(tab.url)
+//        chrome.tabs.executeScript(null, {
+//            file: "content_script.js"
+//        });
+//    }
+//
+//});
+//
+
+    chrome.commands.onCommand.addListener(function (command) {
+        chrome.extension.getBackgroundPage().console.log('Command:', command);
+ chrome.tabs.getSelected(null,function(tab) {
+    copyURL(tab.url)
+});
+   });
