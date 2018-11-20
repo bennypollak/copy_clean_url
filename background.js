13c9deae8832fb7f1fbc5bcab1a26ab995506308
Benny Pollak <yo372002@yahoo.com>
2018-11-20 18:18:38 -0500

    cleanup

--- background.js
+++ background.js
@@ -1,6 +1,4 @@
 // Copyright (c) 2018 Alben Software. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
 
 chrome.commands.onCommand.addListener(function (command) {
--- utils.js
+++ utils.js
@@ -1,8 +1,5 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Copyright (c) 2018 Alben Software. All rights reserved.
+
 let bp = chrome.extension.getBackgroundPage()
 bp.console.log("loading utils")
 
